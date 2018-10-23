647e05716b7231a2e6a4e487f886faa31df13cd9
aleal <aleal@soaint.com>
2018-10-23 13:53:05 -0500

    Respuesta nula de incidente

--- src/java/net/telmex/integration/onyx/ordering.java
+++ src/java/net/telmex/integration/onyx/ordering.java
@@ -32,38 +32,44 @@ public class ordering {
             @XmlElement(required = true) String incidentId, @WebParam(name = "mode") 
             @XmlElement(required = true) String mode) {
         
-        String[] key = {"CODIGO", "TIPO", "NUM_USER", "DESCRIPCION", "FECHAIN", "iIncidentCategory", 
-	"ENLACES", "FECHAMOD", "vchUser1", "vchUser5", "iStatusId", "ESTADOID", 
-	"EMAIL", "CATEGORIA", "chAssignedTo", "PADRE", "PRIORIDAD", "OT", "CUENTA", 
-	"TELEFONO", "vchDesc1", "iOwnerId", "PARAMETROID", "CIUDAD", "NIT", 
-	"USUARIO_ASIGNADO", "DIRECCION", "ESTADO", "iIncidentTypeId", 
-	"COMPANIA", "PAIS", "IT_IN", "CIUDADREAL", "USERNAME"};
-        
-        String[] value = {"101545", "Entrega de Servicio Ins.", "", "Comercio al por menor, en esta", 
-	"2000-07-07 12:00:00.0", "11", "", "2018-09-18 15:52:00.0", "", "",
-	"101465", "101465", "almacenes.exito@exito.com.co", "Documentación", "EGU3728A", 
-	"5836532", "127", "5975363", "13198", "57-4-3396565", "CALARCA // ENTREGA DE SERVICIO", 
-	"13198", "111094", "ANTIOQUIA", "890900608-9", "JONATHAN JAHIR BARRERA GOMEZ", 
-	"Cra. 48  32B Sur-139", "Cerrada", "111094", "ALMACENES EXITO S.A.", "CO", 
-	"CALARCA // ENTREGA DE SERVICIO", "ENVG", "JONATHAN JAHIR BARRERA GOMEZ"};
+        System.out.println("Indecidente: " + incidentId);
+        if (incidentId.equals("1234") || incidentId.equals("0000")) {
+        System.out.println("Indecidente: " + incidentId + " en condicional");
+            
+            String[] key = {"CODIGO", "TIPO", "NUM_USER", "DESCRIPCION", "FECHAIN", "iIncidentCategory", 
+            "ENLACES", "FECHAMOD", "vchUser1", "vchUser5", "iStatusId", "ESTADOID", 
+            "EMAIL", "CATEGORIA", "chAssignedTo", "PADRE", "PRIORIDAD", "OT", "CUENTA", 
+            "TELEFONO", "vchDesc1", "iOwnerId", "PARAMETROID", "CIUDAD", "NIT", 
+            "USUARIO_ASIGNADO", "DIRECCION", "ESTADO", "iIncidentTypeId", 
+            "COMPANIA", "PAIS", "IT_IN", "CIUDADREAL", "USERNAME"};
 
-        IncidentInfoList iil = new IncidentInfoList();
-        Entry e;
-        Fields f = new Fields();
-        ArrayList<Entry> a = new ArrayList();
-        
-        for (int i = 0; i < key.length; i++) {
-            e = new Entry();
-            e.setKey(key[i]);
-            e.setValue(value[i]);
-            a.add(e);
-        }
+            String[] value = {"101545", "Entrega de Servicio Ins.", "", "Comercio al por menor, en esta", 
+            "2000-07-07 12:00:00.0", "11", "", "2018-09-18 15:52:00.0", "", "",
+            "101465", "101465", "almacenes.exito@exito.com.co", "Documentación", "EGU3728A", 
+            "5836532", "127", "5975363", "13198", "57-4-3396565", "CALARCA // ENTREGA DE SERVICIO", 
+            "13198", "111094", "ANTIOQUIA", "890900608-9", "JONATHAN JAHIR BARRERA GOMEZ", 
+            "Cra. 48  32B Sur-139", "Cerrada", "111094", "ALMACENES EXITO S.A.", "CO", 
+            "CALARCA // ENTREGA DE SERVICIO", "ENVG", "JONATHAN JAHIR BARRERA GOMEZ"};
 
-        f.setEntry(a);
-        iil.setFields(f);
-        
-        return iil;
+            IncidentInfoList iil = new IncidentInfoList();
+            Entry e;
+            Fields f = new Fields();
+            ArrayList<Entry> a = new ArrayList();
+
+            for (int i = 0; i < key.length; i++) {
+                e = new Entry();
+                e.setKey(key[i]);
+                e.setValue(value[i]);
+                a.add(e);
+            }
+
+            f.setEntry(a);
+            iil.setFields(f);
+            
+            return iil;            
+        }else 
+            return null;
     }
-
+      
     
 }
